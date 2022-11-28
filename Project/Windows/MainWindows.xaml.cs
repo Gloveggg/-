a8b9217684b4096203d0454181da457037e9f5df
chern <chern@LAPTOP-OCRTQP3Q>
2022-11-28 10:22:03 +0300

    Добавьте файлы проекта.

---
+++ Project/Windows/MainWindows.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Project.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindows.xaml
+    /// </summary>
+    public partial class MainWindows : Window
+    {
+        public MainWindows()
+        {
+            InitializeComponent();
+        }
+
+        private void Record_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrm.Navigate(new RecordPage());
+        }
+
+        private void Klients_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrm.Navigate(new KlientsPage());
+        }
+
+        private void Service_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrm.Navigate(new ServicePage());
+        }
+
+        private void Materials_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrm.Navigate(new MaterialsPage());
+        }
+
+        private void Barber_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrm.Navigate(new BarberPage());
+        }
+    }
+}
---
+++ Project/Windows/StartWindows.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Project.AppData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для StartWindows.xaml
+    /// </summary>
+    public partial class StartWindows : Window
+    {
+        Users users = new Users() { Login = "Admin", Password = "123" };
+        public StartWindows()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(LoginTb.Text) && !string.IsNullOrWhiteSpace(PasswordTb.Password))
+            {
+                MessageBox.Show("Добро пожаловать");
+                MainWindows mainWindows = new MainWindows();
+                mainWindows.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Введите данные");
+            }
+        }
+    }
+}
